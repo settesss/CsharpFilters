56c6e31095b61f1337f36e7bb747a3489d34d011
Nikita <nnmanakovv@gmail.com>
2023-10-26 20:41:20 +0700

    add(project): добавлен проект C#, в котором реализуются базовые фильтры "Контур" и "Инверсия цветов"

---
+++ MainForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Filters
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButtonOpen = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonContour = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonInversion = new System.Windows.Forms.ToolStripButton();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.trackBar = new System.Windows.Forms.TrackBar();
+            this.progressBarLoading = new System.Windows.Forms.ProgressBar();
+            this.toolStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButtonOpen,
+            this.toolStripButtonSave,
+            this.toolStripButtonContour,
+            this.toolStripButtonInversion});
+            this.toolStrip.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(816, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "Меню";
+            // 
+            // toolStripButtonOpen
+            // 
+            this.toolStripButtonOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonOpen.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonOpen.Image")));
+            this.toolStripButtonOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonOpen.Name = "toolStripButtonOpen";
+            this.toolStripButtonOpen.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonOpen.Text = "Загрузить файл";
+            this.toolStripButtonOpen.Click += new System.EventHandler(this.toolStripButtonOpen_Click);
+            // 
+            // toolStripButtonSave
+            // 
+            this.toolStripButtonSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonSave.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonSave.Image")));
+            this.toolStripButtonSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonSave.Name = "toolStripButtonSave";
+            this.toolStripButtonSave.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonSave.Text = "Сохранить картинку как...";
+            this.toolStripButtonSave.Click += new System.EventHandler(this.toolStripButtonSave_Click);
+            // 
+            // toolStripButtonContour
+            // 
+            this.toolStripButtonContour.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonContour.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonContour.Image")));
+            this.toolStripButtonContour.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonContour.Name = "toolStripButtonContour";
+            this.toolStripButtonContour.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonContour.Text = "Контур";
+            this.toolStripButtonContour.Click += new System.EventHandler(this.toolStripButtonContour_Click);
+            // 
+            // toolStripButtonInversion
+            // 
+            this.toolStripButtonInversion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonInversion.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonInversion.Image")));
+            this.toolStripButtonInversion.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonInversion.Name = "toolStripButtonInversion";
+            this.toolStripButtonInversion.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonInversion.Text = "Инверсия цветов";
+            this.toolStripButtonInversion.Click += new System.EventHandler(this.toolStripButtonInversion_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 28);
+            this.pictureBox.MinimumSize = new System.Drawing.Size(200, 200);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(792, 713);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 2;
+            this.pictureBox.TabStop = false;
+            // 
+            // trackBar
+            // 
+            this.trackBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar.LargeChange = 2;
+            this.trackBar.Location = new System.Drawing.Point(12, 747);
+            this.trackBar.Maximum = 50;
+            this.trackBar.Name = "trackBar";
+            this.trackBar.Size = new System.Drawing.Size(792, 45);
+            this.trackBar.SmallChange = 5;
+            this.trackBar.TabIndex = 3;
+            this.trackBar.TickFrequency = 2;
+            this.trackBar.Value = 24;
+            this.trackBar.Scroll += new System.EventHandler(this.trackBar_Scroll);
+            // 
+            // progressBarLoading
+            // 
+            this.progressBarLoading.Location = new System.Drawing.Point(249, 372);
+            this.progressBarLoading.Margin = new System.Windows.Forms.Padding(253, 3, 253, 3);
+            this.progressBarLoading.Name = "progressBarLoading";
+            this.progressBarLoading.Size = new System.Drawing.Size(325, 26);
+            this.progressBarLoading.TabIndex = 4;
+            this.progressBarLoading.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(816, 804);
+            this.Controls.Add(this.progressBarLoading);
+            this.Controls.Add(this.trackBar);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.toolStrip);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Фильтры";
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripButtonOpen;
+        private System.Windows.Forms.ToolStripButton toolStripButtonSave;
+        private System.Windows.Forms.ToolStripButton toolStripButtonContour;
+        private System.Windows.Forms.ToolStripButton toolStripButtonInversion;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.TrackBar trackBar;
+        private System.Windows.Forms.ProgressBar progressBarLoading;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Filters
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// Открытое изображение в <see cref="pictureBox"/>.
+        /// </summary>
+        private OpenFileDialog _ofd = new OpenFileDialog();
+
+        /// <summary>
+        /// Отслеживание нажатия кнопки <see cref="toolStripButtonContour"/>.
+        /// </summary>
+        private bool _isCountourFilter = false;
+
+        /// <summary>
+        /// Инициализация главной формы.
+        /// </summary>
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Преобразует цвет в соответствующий оттенок серого, где
+        /// подразумевается, что 30% красного, 59% зеленого и 11% синего - 
+        /// это взвешенное усреднение в процессе подсчета яркости.
+        /// </summary>
+        /// <param name="oldColor"></param>
+        /// <returns></returns>
+        private Color GrayscaleFilter(Color oldColor)
+        {
+            int luminance = (int)(oldColor.R * 0.3 + oldColor.G * 0.59 + oldColor.B * 0.11);
+
+            return Color.FromArgb(0, luminance, luminance, luminance);
+        }
+
+        /// <summary>
+        /// Преобразует переданный цвет в инвертированный.
+        /// </summary>
+        /// <param name="oldColor"></param>
+        /// <returns></returns>
+        private Color InversionFilter(Color oldColor)
+        {
+            int newRed = 0xFF - oldColor.R;
+            int newGreen = 0xFF - oldColor.G;
+            int newBlue = 0xFF - oldColor.B;
+
+            return Color.FromArgb(0, newRed, newGreen, newBlue);
+        }
+
+        /// <summary>
+        /// Применяет выбранный фильтр, используя один алгоритм преобразования.
+        /// </summary>
+        /// <param name="filterFunction"></param>
+        private void ApplyFilter(Func<Color, Color> filterFunction)
+        {
+            try
+            {
+                Bitmap initialPicture = new Bitmap(_ofd.FileName);
+
+                Bitmap resultPicture = new Bitmap(_ofd.FileName);
+
+                progressBarLoading.Visible = true;
+
+                int Height = initialPicture.Height;
+                int Width = initialPicture.Width;
+
+                for (int x = 0; x < Width - 1; x++)
+                {
+                    for (int y = 0; y < Height - 1; y++)
+                    {
+                        // Если была нажата кнопка контура, рассчитывает градиенты для получения контуров
+                        // Иначе идёт по стандартному алгоритму, используя переданные цвета
+                        if (_isCountourFilter == true)
+                        {
+                            int current = initialPicture.GetPixel(x, y).R;
+                            int up = initialPicture.GetPixel(x + 1, y).R;
+                            int right = initialPicture.GetPixel(x, y + 1).R;
+
+                            int dx = Math.Abs(right - current);
+                            int dy = Math.Abs(up - current);
+
+                            int gradient = (int)Math.Sqrt(Math.Pow(dx, 2) - Math.Pow(dy, 2)) * trackBar.Value;
+
+
+                            if (gradient > 255)
+                            {
+                                gradient = 255;
+                            }
+
+                            Color gradientColor;
+                            gradientColor = Color.FromArgb(0, gradient, gradient, gradient);
+                            resultPicture.SetPixel(x, y, gradientColor);
+                        }
+                        else
+                        {
+                            Color oldColor = initialPicture.GetPixel(x, y);
+                            Color newColor = filterFunction(oldColor);
+                            resultPicture.SetPixel(x, y, newColor);
+                        }
+
+                        double progress = (double)(x * Height + y) / (Width * Height);
+                        progressBarLoading.Value = (int)(progress * 100);
+                    }
+                }
+
+                progressBarLoading.Visible = false;
+
+                _isCountourFilter = false;
+                pictureBox.Image = resultPicture;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void toolStripButtonOpen_Click(object sender, EventArgs e)
+        {   
+            _ofd.Filter = "Image Files(*.PNG;*.JPG;*.BMP)|*.PNG;*.JPG;*.BMP";
+
+            if (_ofd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox.Image = new Bitmap(_ofd.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Невозможно открыть выбранный файл.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void toolStripButtonSave_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+
+                sfd.OverwritePrompt = true;
+                sfd.CheckPathExists = true;
+                sfd.Filter = "JPG(*.JPG)|*.JPG|PNG(*.PNG)|*.PNG|BMP(*.BMP)|*.BMP";
+                sfd.ShowHelp = true;
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        pictureBox.Image.Save(sfd.FileName);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void toolStripButtonContour_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                _isCountourFilter = true;
+                ApplyFilter(GrayscaleFilter);
+            }
+        }
+
+        private void toolStripButtonInversion_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image != null)
+            {
+                ApplyFilter(InversionFilter);
+            }
+        }
+
+        private void trackBar_Scroll(object sender, EventArgs e)
+        {
+            int value = trackBar.Value;
+            if (value % 2 != 0)
+            {
+                // Округление значения до ближайшего четного числа
+                int newValue = (value % 2 == 0) ? value : value + 1;
+                trackBar.Value = newValue;
+            }
+        }
+    }
+}
